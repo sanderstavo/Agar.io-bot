fb3447d97b0711fe4ac5f66d241eb41cdd32047c
sanderstavo <48795909+sanderstavo@users.noreply.github.com>
2019-03-21 14:15:36 +0100

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -23,7 +23,7 @@ SOFTWARE.*/
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/*
+// @include     http://agar.io/RVQCYU
 // @version     3.651
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
